5f3d0c3c7c171ea5fb3df7a38f2635fcf590ac2d
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-06-03 16:34:54 +0200

    refactor workflow

--- src/demo_temporal/workflows/inference/activities.py
+++ src/demo_temporal/workflows/inference/activities.py
@@ -1,13 +1,27 @@
-import asyncio
+from .llm import get_model_async
 from temporalio import activity
 
 from demo_temporal.workflows.shared import InferenceInput
 
 
 @activity.defn
-async def run_inference(inference_input: InferenceInput) -> str:
-    import numpy as np
+async def get_model(model_path: str, model_url: str = None) -> dict:
+    await get_model_async(model_path, model_url)
 
-    result = np.add(1, 4)
-    await asyncio.sleep(60)
-    return f"{inference_input.model_name} - {inference_input.file} - {result}"
+
+@activity.defn
+async def construct_model_input(model_input: str, input_file: str) -> str:
+    # also validates that the input is not empty
+    if not model_input:
+        with open(input_file, "r", encoding="utf-8") as f:
+            model_input = f.read()
+    if not model_input:
+        raise ValueError("Input data cannot be empty.")
+
+    return model_input
+
+
+@activity.defn
+async def run_inference(model_path, model_input) -> dict:
+    # load the model and run inference
+    pass
--- src/demo_temporal/workflows/inference/workflow.py
+++ src/demo_temporal/workflows/inference/workflow.py
@@ -3,7 +3,11 @@ from datetime import timedelta
 from temporalio import workflow
 
 with workflow.unsafe.imports_passed_through():
-    from demo_temporal.workflows.inference.activities import run_inference
+    from demo_temporal.workflows.inference.activities import (
+        construct_model_input,
+        get_model,
+        run_inference,
+    )
     from demo_temporal.workflows.shared import InferenceInput
 
 
@@ -12,7 +16,20 @@ class InferenceWorkflow:
     @workflow.run
     async def run(self, data: InferenceInput) -> None:
         await workflow.execute_activity(
+            get_model,
+            data.model_path,
+            data.model_url,
+            start_to_close_timeout=timedelta(seconds=120),
+        )
+        model_input = await workflow.execute_activity(
+            construct_model_input,
+            data.input,
+            data.input_file,
+            start_to_close_timeout=timedelta(seconds=60),
+        )
+        await workflow.execute_activity(
             run_inference,
-            data,
+            data.model_path,
+            model_input,
             start_to_close_timeout=timedelta(seconds=120),
         )
