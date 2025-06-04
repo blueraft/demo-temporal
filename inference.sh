b1775daada56b36e3df938d5e415726027c8f071
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-06-04 15:01:06 +0200

    generate cifs too

--- inference.sh
+++ inference.sh
@@ -3,5 +3,6 @@ curl -X POST http://localhost:8000/start-inference-task\
     -d '{"model_path": "models/crystallm_v1_small/ckpt.pt",
          "model_url": "https://zenodo.org/records/10642388/files/crystallm_v1_small.tar.gz",
          "raw_input": "data_Li1Mn1O2",
-         "attempts": "1"
+         "attempts": "1",
+         "generate_cif": "true"
          }'\
